b395141bff72751822854ca857382cdee0ab9353
Paco Delhumeau <pacodelhumeau@gmail.com>
2021-09-15 01:05:39 -0500

    Añadido git pull en el archivo batch para iniciar programa / Versión 3.0.9

--- Desktop/StartService.bat
+++ Desktop/StartService.bat
@@ -1,2 +1,5 @@
-START C:\xampp\xampp_start.exe
-"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "https://localhost/MicroarreglosDNA/"
+@echo off
+cd C:\xampp\htdocs\MicroarreglosDNA
+git pull
+START /min C:\xampp\xampp_start.exe
+"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://localhost/MicroarreglosDNA/"
