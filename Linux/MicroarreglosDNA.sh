1795e22d8b62e500c133b706b62459044a32c0ec
frankfsa6 <frankfsa6@gmail.com>
2021-10-03 23:56:34 -0500

    Versión 4.12

--- Linux/MicroarreglosDNA.sh
+++ Linux/MicroarreglosDNA.sh
@@ -5,21 +5,21 @@ if test -f ~/Downloads/tempDNA.txt; then
     echo .
     read -t 3 -p "MicroarreglosDNA ya está funcionando..."
     exit 1
-else
-    echo "tempDNA">~/Downloads/tempDNA.txt
-    sudo apt-get install zenity -y
-    zenity --info --title="Microarreglos DNA" --width=400 --text="Se descargará la última versión del programa e iniciará automáticamente."
-    cd /var/www/html/MicroarreglosDNA
-    sudo git reset --hard origin/master
-    sudo git pull
-    #Crea accesos directos
-    sudo mkdir ~/Desktop/MicroarreglosDNA
-    sudo cp -u /var/www/html/MicroarreglosDNA/Linux/MicroarreglosDNA.sh ~/Desktop/MicroarreglosDNA/
-    sudo cp -u /var/www/html/MicroarreglosDNA/Linux/*.url ~/Desktop/MicroarreglosDNA/
-    sudo chmod -R -f 0777 ~/Desktop/MicroarreglosDNA
-    #Abre página de internet y finaliza programa
-    sudo service apache2 restart
-    DISPLAY=:0 chromium-browser "http://localhost/MicroarreglosDNA/"
-    xdg-open "http://localhost/MicroarreglosDNA/"
-    sudo rm -r ~/Downloads/tempDNA.txt
 fi
+#Actualiza repositorio
+echo "tempDNA">~/Downloads/tempDNA.txt
+sudo apt-get install zenity -y
+zenity --info --title="Microarreglos DNA" --width=400 --text="Se descargará la última versión del programa e iniciará automáticamente."
+cd /var/www/html/MicroarreglosDNA
+sudo git reset --hard origin/master
+sudo git pull
+#Crea accesos directos
+sudo mkdir ~/Desktop/MicroarreglosDNA
+sudo cp -u /var/www/html/MicroarreglosDNA/Linux/MicroarreglosDNA.sh ~/Desktop/MicroarreglosDNA/
+sudo cp -u /var/www/html/MicroarreglosDNA/Linux/*.url ~/Desktop/MicroarreglosDNA/
+sudo chmod -R -f 0777 ~/Desktop/MicroarreglosDNA
+#Abre página de internet y finaliza programa
+sudo service apache2 restart
+DISPLAY=:0 chromium-browser "http://localhost/MicroarreglosDNA/"
+xdg-open "http://localhost/MicroarreglosDNA/"
+sudo rm -r ~/Downloads/tempDNA.txt
--- Windows/MicroarreglosDNA.bat
+++ Windows/MicroarreglosDNA.bat
@@ -7,25 +7,24 @@ if exist %temp%\MicroDNA.vbs (
     echo .
     echo  MicroarreglosDNA ya está funcionando...
     timeout 3
+    exit 1
 )
-else (
-    echo MsgBox "Los servicios se van a actualizar e iniciar inmediatamente.", 64, "Microarreglos DNA">%temp%\MicroDNA.vbs
-    start %temp%\MicroDNA.vbs
-    cd C:\xampp\htdocs\MicroarreglosDNA
-    git reset --hard origin/master
-    git pull
-    mkdir %userprofile%\Desktop\MicroarreglosDNA
-    copy /y C:\xampp\htdocs\MicroarreglosDNA\Windows\MicroarreglosDNA.bat %userprofile%\Desktop\MicroarreglosDNA\
-    copy /y C:\xampp\htdocs\MicroarreglosDNA\Windows\*.url %userprofile%\Desktop\MicroarreglosDNA\
-    start /b C:\xampp\xampp_start.exe
-    start ""  http://localhost/MicroarreglosDNA/
-    cls
-    echo .
-    echo  MicroarreglosDNA ya está funcionando, presione cualquier tecla para terminar el programa y sus servicios...
-    pause
-    start /b C:\xampp\xampp_stop.exe
-    echo MsgBox "Servicios del programa finalizados.", 64, "Microarreglos DNA">%temp%\MicroDNA.vbs
-    cscript %temp%\MicroDNA.vbs
-    del %temp%\MicroDNA.vbs
-)
+echo MsgBox "Los servicios se van a actualizar e iniciar inmediatamente.", 64, "Microarreglos DNA">%temp%\MicroDNA.vbs
+start %temp%\MicroDNA.vbs
+cd C:\xampp\htdocs\MicroarreglosDNA
+git reset --hard origin/master
+git pull
+mkdir %userprofile%\Desktop\MicroarreglosDNA
+copy /y C:\xampp\htdocs\MicroarreglosDNA\Windows\MicroarreglosDNA.bat %userprofile%\Desktop\MicroarreglosDNA\
+copy /y C:\xampp\htdocs\MicroarreglosDNA\Windows\*.url %userprofile%\Desktop\MicroarreglosDNA\
+start /b C:\xampp\xampp_start.exe
+start ""  http://localhost/MicroarreglosDNA/
+cls
+echo .
+echo  MicroarreglosDNA ya está funcionando, presione cualquier tecla para terminar el programa y sus servicios...
+pause
+start /b C:\xampp\xampp_stop.exe
+echo MsgBox "Servicios del programa finalizados.", 64, "Microarreglos DNA">%temp%\MicroDNA.vbs
+cscript %temp%\MicroDNA.vbs
+del %temp%\MicroDNA.vbs
 exit 1
